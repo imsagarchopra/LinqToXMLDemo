2cfd2f5acd5f4e1805f1896dbb89bb581367338b
Sagar Chopra <Chopra.sagar89@gmail.com>
2021-01-12 15:01:31 +0530

    Add project files.

---
+++ Creating XML Document/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Creating_XML_Document
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xmlDocument = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+
+                new XComment("Creating an XML Tree using LINQ to XML"),
+
+                new XElement("Students",
+
+                    new XElement("Student", new XAttribute("Id", 101),
+                        new XElement("Name", "Mark"),
+                        new XElement("Gender", "Male"),
+                        new XElement("TotalMarks", 800)),
+
+                    new XElement("Student", new XAttribute("Id", 102),
+                        new XElement("Name", "Rosy"),
+                        new XElement("Gender", "Female"),
+                        new XElement("TotalMarks", 900)),
+
+                    new XElement("Student", new XAttribute("Id", 103),
+                        new XElement("Name", "Pam"),
+                        new XElement("Gender", "Female"),
+                        new XElement("TotalMarks", 850)),
+
+                    new XElement("Student", new XAttribute("Id", 104),
+                        new XElement("Name", "John"),
+                        new XElement("Gender", "Male"),
+                        new XElement("TotalMarks", 950))));
+
+            xmlDocument.Save(@"C:\Users\dell\source\repos\LinqToXMLDemo\Creating XML Document\Data.xml");
+
+        }
+    }
+}
