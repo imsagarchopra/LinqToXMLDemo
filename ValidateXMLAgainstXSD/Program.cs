fc0aebb7413a148a5d8a45b3203cc569863d46df
Sagar Chopra <Chopra.sagar89@gmail.com>
2021-01-16 11:52:00 +0530

    Validating XML against XSD

---
+++ ValidateXMLAgainstXSD/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace ValidateXMLAgainstXSD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSchemaSet schema = new XmlSchemaSet();
+            schema.Add("", @"C:\Users\dell\source\repos\LinqToXMLDemo\ValidateXMLAgainstXSD\Student.xsd");
+
+            XDocument xmlDocument = XDocument.Load(@"C:\Users\dell\source\repos\LinqToXMLDemo\ValidateXMLAgainstXSD\Data.xml");
+
+            bool validationErrors = false;
+
+            xmlDocument.Validate(schema, (s, e) =>
+            {
+                Console.WriteLine(e.Message);
+                validationErrors = true;
+            });
+
+            if (validationErrors)
+                Console.WriteLine("Validation Failed");
+            else
+                Console.WriteLine("Validation Succeeded");
+        }
+    }
+}
